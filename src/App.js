e5a1e3ba470557ba696a46fa509bd502e7f03e2b
ElGhamryy <abdelrahmanghamryy@gmail.com>
2024-04-04 06:20:31 +0200

    uploading files to repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef();
+
+  const handleAddButton = () => {
+    const text = inputRef.current.value
+    if (text === "")
+    {
+      return
+    }
+    const newTodo = {text, completed: false}
+    setTodos([...todos, newTodo])
+    inputRef.current.value = '';
+  }
+
+  const handleTaskDone = (index) => {
+    const newTodos = [...todos]
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  }
+
+  const handleDeleteButton = (index) => {
+    setTodos( () => {
+      return todos.filter((todo) => todos.indexOf(todo) !== index)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <span className='header'>To Do</span>
+        <input ref={inputRef} placeholder="Add Task.."/>
+        <button className='add-button' onClick={handleAddButton}>Add</button>
+        
+        <ul>
+          {todos.map(({text, completed}, index) => {
+            return( 
+              <div className='item'>
+                <li key={index} className={completed ? "done" : ""} onClick={() => handleTaskDone(index)}>{text}</li>
+                <button onClick={() => handleDeleteButton(index)}>❌</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
